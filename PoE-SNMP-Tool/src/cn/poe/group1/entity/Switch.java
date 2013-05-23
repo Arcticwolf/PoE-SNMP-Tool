2728e13bcc5591b5d201f86d4791508bcfc602e8
Felix Mayerhuber <felix.mayerhuber@gmail.com>
2013-05-23 22:29:58 +0200

    memory leak is now less big

--- PoE-SNMP-Tool/src/cn/poe/group1/entity/Switch.java
+++ PoE-SNMP-Tool/src/cn/poe/group1/entity/Switch.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -23,7 +24,7 @@ public class Switch implements Serializable {
     private String type;
     private int portCount;
     private String comment;
-    @OneToMany(mappedBy="sw", cascade=CascadeType.ALL)
+    @OneToMany(mappedBy="sw", cascade=CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Port> ports;
     
     public Switch() {
