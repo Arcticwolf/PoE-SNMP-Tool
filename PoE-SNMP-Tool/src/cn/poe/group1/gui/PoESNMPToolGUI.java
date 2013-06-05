f9893f852b7e819fb0046758ceff3dcb799776dc
sauron <sauron@mordor.(none)>
2013-06-05 18:54:49 +0200

    documentation erweitert

--- PoE-SNMP-Tool/src/cn/poe/group1/gui/PoESNMPToolGUI.java
+++ PoE-SNMP-Tool/src/cn/poe/group1/gui/PoESNMPToolGUI.java
@@ -689,7 +689,7 @@ public class PoESNMPToolGUI extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1024, Short.MAX_VALUE)
+            .addGap(0, 1025, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
