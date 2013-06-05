0106f4f97b32920a4288a55bda19e7ebb3963c7f
Christopher Morrent <christopher.morrent@student.tuwien.ac.at>
2013-06-05 17:34:21 +0200

    Updated DataRetriever for correct MIB port-handling

--- PoE-SNMP-Tool/src/cn/poe/group1/Main.java
+++ PoE-SNMP-Tool/src/cn/poe/group1/Main.java
@@ -57,14 +57,15 @@ public class Main {
     
     private void addTestSwitches() {
         // Just for testing impact of one MeasurementDatabase per thread
-        for (int i=0; i<5; i++) {
-            Switch sw = new Switch("testid_"+String.valueOf(i), "testIp", "testtype", 24, "testswitch");
-            for (int j=0; j<sw.getPortCount(); j++) {
+        //for (int i=0; i<5; i++) {
+            //Switch sw = new Switch("testid_"+String.valueOf(i), "testIp", "testtype", 24, "testswitch");
+            Switch sw = new Switch("testid", "128.131.30.85", "testtype", 48, "testswitch");
+            for (int j=1; j<=sw.getPortCount(); j++) {
                 Port p = new Port(sw, j, null);
                 sw.addPort(p);
             }
             measurementBackend.persistSwitch(sw);
-        }
+        //}
     }
     
     public MeasurementBackend getBackend() {
